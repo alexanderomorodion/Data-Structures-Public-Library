58895347f21945f08b5f9577be7d7720c7b0174a
Kayla Krigbaum <krigbaum.kayla8@gmail.com>
2019-10-30 14:24:24 -0500

    Adding / fixing documentation in Employee.cpp

--- Data Structures Library/Data Structures Library/Employee.cpp
+++ Data Structures Library/Data Structures Library/Employee.cpp
@@ -1,14 +1,14 @@
 #include "Employee.h"
 
 //Default constructor which sets the employee name to an empty string
-//Big O: O(n)
+//Big O: O(1)
 Employee::Employee()
 {
 	name = "";
 }
 
 //Constructor which takes the user name, the new wait time, and the new retain time and sets those variables
-//Big O: O(n)
+//Big O: O(1)
 Employee::Employee(string userName, int newWait, int newRetain)
 {
 	name = userName;
@@ -17,42 +17,42 @@ Employee::Employee(string userName, int newWait, int newRetain)
 }
 
 //Takes the users name and sets it
-//Big O: O(n)
+//Big O: O(1)
 void Employee::setName(string userName)
 {
 	name = userName;
 }
 
 //Takes the users wait time and sets it
-//Big O: O(n)
+//Big O: O(1)
 void Employee::setWait(int userWait)
 {
 	waitTime = userWait;
 }
 
 //Takes the users retain time and sets it
-//Big O: O(n)
+//Big O: O(1)
 void Employee::setRetain(int userRetain)
 {
 	retainTime = userRetain;
 }
 
 //Returns the users name when called
-//Big O: O(n)
+//Big O: O(1)
 string Employee::getName()
 {
 	return name;
 }
 
 //Returns the users wait time when called
-//Big O: O(n)
+//Big O: O(1)
 int Employee::getWait()
 {
 	return waitTime;
 }
 
 //Returns the users retain time when called
-//Big O: O(n)
+//Big O: O(1)
 int Employee::getRetain()
 {
 	return retainTime;
